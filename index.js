74857506d5d99ad7dce0c1c6dca8aba930dfdf7e
Thomas Wieck <thomas.wieck@hotmail.co.uk>
2024-10-04 20:31:42 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// sticker.style.setProperty('--rotation', Math.random() +'deg');
+
+window.onload = function () {
+  // Get the viewport width and height
+  const viewportWidth = window.innerWidth;
+  const viewportHeight = window.innerHeight;
+
+  const textHeight = 100;
+
+  const max = 60;
+  const min = -55;
+
+  const sticker = document.getElementsByClassName("sticker");
+
+  Array.from(sticker).map((element) => {
+    const rotation = Math.random() * (max - min) + min;;
+    element.style.transform = `rotate(${rotation}deg)`;
+
+    // Generate random values for top and left (inside viewport bounds)
+    const randomTop = Math.floor(
+      Math.random() * (viewportHeight - element.offsetHeight)
+    );
+    const randomLeft = Math.floor(
+      Math.random() * (viewportWidth - element.offsetWidth)
+    );
+
+    element.style.zIndex = `${Math.floor(Math.random() * (3 - 1) + 1)}`;
+
+    // Set the random top and left positions
+    element.style.top = `${randomTop + textHeight}px`;
+    element.style.left = `${randomLeft}px`;
+  });  
+};
